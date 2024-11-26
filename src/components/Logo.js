ef1ec50024e7919dc2bda310ac46a0ebce8440ef
Mark Yu · 于震潇 <zyu347@uwo.ca>
2024-11-25 20:35:39 -0500

    logo update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,16 +4,16 @@ import logoSvg from "../assets/logo.svg";
 
 const Logo = () => {
   return (
-    <Link
-      to="/"
-      className="
-     absolute top-[1.5rem] left-[1.5rem] [text-decoration:none]
-text-lg text-cyan flex items-center
-     "
-    >
-      <img src={logoSvg} alt="CryptoBucks" />
-      <span>M4rketView</span>
-    </Link>
+      <Link
+          to="/"
+          className="
+        absolute top-[1.5rem] left-[1.5rem] no-underline
+        flex items-center text-lg text-cyan
+      "
+      >
+        <img src={logoSvg} alt="logo" className="m-3 w-8 h-8" />
+        <span className="font-semibold">M4rketView</span>
+      </Link>
   );
 };
 
